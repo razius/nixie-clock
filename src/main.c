e82ae61ae299c8afd0e935d56c91e18acf38cf07
Silviu Tantos <me@razius.com>
2015-09-12 23:51:00 +0200

    Toggle led every second using a timer.

--- src/main.c
+++ src/main.c
@@ -9,9 +9,21 @@
 #include <avr/interrupt.h>
 
 int main(void){
-    // Set PH0 to output and turn all leds on.
-    DDRH = 0xFF;
-    PORTH = 0x00;
+    // Clear Timer on Compare Match (CTC) Mode with OCRnA as top.
+    TCCR4B |= _BV(WGM42);
+
+    // Toggle OCnA on compare match
+    TCCR4A |= _BV(COM4A0);
+
+    // clk / (2 * prescaler * (1 + OCRnA))
+    OCR4AH = 0x7A;
+    OCR4AL = 0x12;
+
+    // Setup Timer 0 pre-scaler to clk/256
+    TCCR4B |= _BV(CS42);
+
+    // Setup PH3 to output
+    DDRH |= _BV(DDH3);
 
     // Enable MCU interrupt (set I-flag)
     sei();
