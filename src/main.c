3423a5f810efdc0125b4a25e74ecb5c6a4b31e5f
Silviu Tantos <me@razius.com>
2015-09-14 22:31:21 +0200

    Increment seconds and show them using LED's.

--- src/main.c
+++ src/main.c
@@ -8,28 +8,43 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+uint8_t SECONDS = 0x00;
+
 int main(void){
     // Clear Timer on Compare Match (CTC) Mode with OCRnA as top.
-    TCCR4B |= _BV(WGM42);
-
-    // Toggle OCnA on compare match
-    TCCR4A |= _BV(COM4A0);
+    TCCR1A |= _BV(WGM12);
 
     // clk / (2 * prescaler * (1 + OCRnA))
-    OCR4AH = 0x7A;
-    OCR4AL = 0x12;
+    OCR1AH = 0x7A;
+    OCR1AL = 0x12;
 
     // Setup Timer 0 pre-scaler to clk/256
-    TCCR4B |= _BV(CS42);
+    TCCR1B |= _BV(CS12);
+
+    // Enable Timer/Counter4 overflow interrupt.
+    TIMSK1 = _BV(TOIE1);
 
     // Setup PH3 to output
-    DDRH |= _BV(DDH3);
+    DDRH = 0xFF;
 
     // Enable MCU interrupt (set I-flag)
     sei();
 
     while (1) {
+        PORTH = ~SECONDS;
     }
 
     return 0;
+}
+
+ISR(TIMER1_OVF_vect){
+    // Setup PH3 to output
+    // DDRH |= _BV(DDH3);
+    // PORTH = PORTH << 1;
+    if (SECONDS < 60) {
+        SECONDS++;
+    }
+    else {
+        SECONDS = 0x00;
+    }
 }
